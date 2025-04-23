12538da95f87b7b8069e03b871a706ef977441fc
ziz4aa <mufidahummahazizah@gmail.com>
2025-04-23 14:49:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 zizaaum ")
+st.title("zizaaum")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
