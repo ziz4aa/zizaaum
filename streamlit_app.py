41f2928caa7e3d8904979958452ef2ccadebaac4
ziz4aa <mufidahummahazizah@gmail.com>
2025-05-21 15:06:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ aimport streamlit as st
 
 st.title("zizaaum")
 st.write(
-    "pacar Song Kang 💋🔥."
-)
+    "pacar Song Kang 💋🔥.")
 st.header("pacar aku")
 st.image("53c845ff78166a2610d9f4173c7dae36.jpg")
 
